2c171b17fbef662e524fa0e79957339afd8f08d9
Adam-Doria <drici.adam@gmail.com>
2025-04-19 23:46:23 +0200

    Exercise 1.2 :  refactor content creating Part component

--- Part-1/Information/src/App.jsx
+++ Part-1/Information/src/App.jsx
@@ -5,16 +5,18 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part part={props.part1} exercises={props.exercices1} />
+      <Part part={props.part1} exercises={props.exercices1} />
+      <Part part={props.part1} exercises={props.exercices1} />
     </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
   )
 }
 
