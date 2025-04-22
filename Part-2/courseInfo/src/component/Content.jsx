46b02cd01fb03f4927f598156aab17874451515b
Adam-Doria <drici.adam@gmail.com>
2025-04-22 21:18:15 +0200

    Exercises 2.2-2.3 : Add sum of exercice using reduce method

--- Part-2/courseInfo/src/component/Content.jsx
+++ Part-2/courseInfo/src/component/Content.jsx
@@ -1,7 +1,17 @@
 import { Part } from './Part'
+import { Total } from './Total'
 
 export const Content = ({ parts }) => {
-  return parts.map((part) => (
-    <Part key={part.id} name={part.name} exercises={part.exercises} />
-  ))
+const totalExersises = parts.reduce((acc,part)=> acc + part.exercises,0)
+
+
+
+  return (
+    <>
+      {parts.map((part) => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      ))}
+      <Total total={totalExersises}/>
+    </>
+  )
 }
