0141da53fde089ab4ccdc6ca036e17f35872a016
Adam-Doria <drici.adam@gmail.com>
2025-04-23 21:07:23 +0200

    exercise 2.7 : add altert if contact already exist

--- Part-2/phoneBook/src/App.jsx
+++ Part-2/phoneBook/src/App.jsx
@@ -4,17 +4,31 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
   const [newName, setNewName] = useState('')
 
-  const handleName = (event) => {
+  const handleChange = (event) => {
     event.preventDefault()
-
-    setNewName([event.target.value])
+    setNewName(event.target.value)
   }
 
+
   const addContact = (event) => {
+
     event.preventDefault()
+    if (! newName) return
+
     const person = {
       name: newName,
     }
+
+    const isContactAlreadyExist = persons.find(
+      (person) => {
+        console.log( person.name, newName)
+        return person.name === newName}
+    )
+
+    if (isContactAlreadyExist) {
+      alert(`${newName} is already added to phonebook`)
+    }
+
     setPersons([...persons, person])
     setNewName('')
   }
@@ -24,7 +38,7 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addContact}>
         <div>
-          name: <input value={newName} onChange={handleName} />
+          name: <input value={newName} onChange={handleChange} />
         </div>
         <div>
           <button type="submit">add</button>
