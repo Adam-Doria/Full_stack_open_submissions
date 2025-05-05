d05f81abfe37c5cdb81f67f25376f256852eb6e4
Adam-Doria <drici.adam@gmail.com>
2025-05-05 21:22:49 +0200

    3.7 : add morgan middleware

--- Part-3/phonebook-backend/index.js
+++ Part-3/phonebook-backend/index.js
@@ -1,9 +1,10 @@
 import express from 'express'
+import morgan from 'morgan'
 
 const app = express()
 app.use(express.json())
+app.use(morgan('tiny'))
 const port = 3001
-
 let contacts = [
   {
     id: '1',
